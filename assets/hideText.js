24f2ff979305a58016f1cae304ea689aafa6e72c
Javier107 <43627978+Javier107@users.noreply.github.com>
2018-09-27 10:39:12 -0300

    Delete hideText.js

--- assets/hideText.js
+++
@@ -1,8 +0,0 @@
-function hideText() {
-    var textC = document.getElementById("commandText").value;
-    if(document.getElementById("commandText").value == ''){
-	  $('#commandText').hide();
-    }else{
-	  $('#commandText').show();
-	}
-}
