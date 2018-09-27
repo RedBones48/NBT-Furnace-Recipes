e111fe98548edf9c0b70601e73acde5a1bcc5d02
Javier107 <43627978+Javier107@users.noreply.github.com>
2018-09-27 13:53:56 -0300

    Update credits.js

--- assets/credits.js
+++ assets/credits.js
@@ -2,4 +2,5 @@ function credits() {
     alert("Credits : Generator By Javier107 (me)");
 	alert("Credits : NBT Furnace Recipes By Javier107 (me)");
 	alert("Credits : API jQuery v2.1.1 | (c) 2005, 2014 jQuery Foundation, Inc. | jquery.org/license");
+	alert("Credits : Helped coding textareas Alex³ and DolphinTech");
 }
