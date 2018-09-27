8f0bc11308c80294d2aa29fdb925d4db161b6558
Javier107 <43627978+Javier107@users.noreply.github.com>
2018-09-27 12:57:12 -0300

    Update command.js

--- assets/command.js
+++ assets/command.js
@@ -50,7 +50,13 @@ function command() {
 	  var a = document.getElementById("itemInIDText").value;
 	  var a1 = text1+''+space+'|'+space+'|'+space2+''+text3+'{Items:[{Slot:0b,id:"minecraft:'+a+'"'+b1+'}]} run scoreboard players set @s nf_item_in '+e+''+space+'|'+space+'|'+space2+'execute if block ~ ~ ~ minecraft:furnace{Items:[{Slot:2b,id:"minecraft:'+c+'"'+d1+'}]} run scoreboard players set @s nf_item_out '+e+''+space+'|'+space+'|'+space2+''+text2+''+space+'|'+space+'|'+space2+'execute if score @s nf_item_in matches '+e+' run replaceitem block ~ ~ ~ container.2 '+c+''+f1
 	}
-    $('#commandText').hide();
+    if(document.getElementById("itemInIDText").value == ''" || 
+       document.getElementById("itemOutIDText").value == ''" || 
+       document.getElementById("recipeIDText").value == ''")  {
+           $('#commandText').hide();
+    } else {
+           $('#commandText').show();
+}
 }
 
 function function_detectText() {
