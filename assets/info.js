95dffccc724e54ffeab69ea9d58855f0967747a4
Javier107 <43627978+Javier107@users.noreply.github.com>
2019-02-13 23:13:40 -0300

    Update info.js

--- assets/info.js
+++ assets/info.js
@@ -1,6 +1,6 @@
 function info() {
     alert("Important Info!");
-	alert("1-Make sure the ID you use for the recipe is not being used in another Recipe you made or that is not the number 0,1,2 or 99999999");
+	alert("1-Make sure the ID you use for the recipe is not being used in another Recipe you made or that is not the number 0,-1 or -2");
 	alert("2-Make sure the Item/Block you use as INGREDIENT is not used already by minecraft in the game , the RESULT can be any item, don't matter if Minecraft has a recipe where that same item is a RESULT too (If you wanna use a Item/Block as INGREDIENT that Minecraft already use you will have to disable Vanilla Minecraft Datapack");
 	alert("3-Be sure to fill the text boxes that are required");
 	alert("4-Make sure that if you use NBT it does not contain the Count tag");
