7307f74294c74f608720c8fe0e2638be91e9221b
Javier107 <43627978+Javier107@users.noreply.github.com>
2019-02-14 23:47:53 -0300

    Update credits.js

--- assets/credits.js
+++ assets/credits.js
@@ -3,6 +3,6 @@ function credits() {
 	alert("Credits : NBT Furnace Recipes By Javier107 (me)");
 	alert("Credits : Furnaces textures By Mojang");
 	alert("Credits : Minecraft By Mojang");
-	alert("Credits : Helped coding textareas Alex³, MulverineX and DolphinTech");
+	alert("Credits : Helped coding Alex³, MulverineX and DolphinTech");
 	alert("Credits : API jQuery v2.1.1 | (c) 2005, 2014 jQuery Foundation, Inc. | jquery.org/license");
 }
